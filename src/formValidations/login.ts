44e50793f276170296cc89c2aa9dbf6047f156df
Gustavo <guhcostan@gmail.com>
2021-12-12 18:14:01 -0300

    pt-br validations

--- src/formValidations/login.ts
+++ src/formValidations/login.ts
@@ -1,8 +1,8 @@
 import * as yup from 'yup';
 
 const loginSchema = yup.object().shape({
-  email: yup.string().email().required(),
-  password: yup.string().min(6).required(),
+  email: yup.string().email().required('O campo email é obrigatório'),
+  password: yup.string().min(6).required('O campo senha é obrigatório'),
 });
 
 export default loginSchema;
--- src/formValidations/register.ts
+++ src/formValidations/register.ts
@@ -1,11 +1,17 @@
 import * as yup from 'yup';
 
 const registerSchema = yup.object().shape({
-  name: yup.string().required(),
-  cpf: yup.string().min(12).required(),
-  email: yup.string().email().required(),
-  password: yup.string().min(6).required(),
-  passwordConfirmation: yup.string().min(6).required(),
+  name: yup.string().required('O campo nome é obrigatório'),
+  cpf: yup
+    .string()
+    .min(12, 'Digite um cpf válido')
+    .required('O campo cpf é obrigatório'),
+  email: yup.string().email().required('O campo email é obrigatório'),
+  password: yup.string().min(6).required('O campo senha é obrigatório'),
+  passwordConfirmation: yup
+    .string()
+    .min(6)
+    .required('Porfavor, escreva novamente a sua senha'),
 });
 
 export default registerSchema;
